ade8492c0228bda5afed2e196e24d3041eb628fe
Roberto Barbosa <robertojf@gmail.com>
2021-04-02 19:21:06 -0300

    Add new project

---
+++ projects/anutils/src/lib/anutils.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { AnutilsComponent } from './anutils.component';
+
+
+
+@NgModule({
+  declarations: [AnutilsComponent],
+  imports: [
+  ],
+  exports: [AnutilsComponent]
+})
+export class AnutilsModule { }
---
+++ projects/anutils/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of anutils
+ */
+
+export * from './lib/anutils.service';
+export * from './lib/anutils.component';
+export * from './lib/anutils.module';

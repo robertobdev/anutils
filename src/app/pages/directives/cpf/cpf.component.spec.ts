a8f57142052b6b39d279313b5f7b082da487cb37
Roberto Barbosa <robertojf@gmail.com>
2021-04-05 20:38:53 -0300

    Add cpf component to example

---
+++ src/app/pages/directives/cpf/cpf.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CpfComponent } from './cpf.component';
+
+describe('CpfComponent', () => {
+  let component: CpfComponent;
+  let fixture: ComponentFixture<CpfComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CpfComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CpfComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

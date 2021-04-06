172a57e8247e85c1ebd8830b4f26c903be37da52
Roberto Barbosa <robertojf@gmail.com>
2021-04-06 20:24:43 -0300

    Add codeViewer component

---
+++ src/app/shared/components/code-viewer/code-viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CodeViewer } from '../../interfaces/code-viewer.interface';
+
+@Component({
+  selector: 'app-code-viewer',
+  templateUrl: './code-viewer.component.html',
+  styleUrls: ['./code-viewer.component.scss'],
+})
+export class CodeViewerComponent implements OnInit {
+  @Input() codeViewers: CodeViewer[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}

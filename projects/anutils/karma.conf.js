97da54de11607a3bfcaed971c974fab47f7d4b6c
Roberto Barbosa <robertojf@gmail.com>
2021-04-08 21:35:21 -0300

    Try to fix code coverage

--- projects/anutils/karma.conf.js
+++ projects/anutils/karma.conf.js
@@ -13,16 +13,7 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
       dir: require('path').join(__dirname, '../../coverage/anutils'),
@@ -39,7 +30,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadlessNoSandbox'],
     singleRun: false,
     restartOnFileChange: true,
     customLaunchers: {
@@ -50,6 +41,7 @@ module.exports = function (config) {
           '--disable-extensions',
           '--remote-debugging-port=9223',
           '--no-sandbox',
+          '--disable-gpu',
         ],
       },
     },

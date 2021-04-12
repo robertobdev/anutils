3a5a034f8d9c867382a837ede45cf8d0a506b961
Roberto Barbosa <robertojf@gmail.com>
2021-04-12 20:55:17 -0300

    Fix lint

--- src/app/pages/validators/email/email.component.ts
+++ src/app/pages/validators/email/email.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ValidateEmail } from 'projects/anutils/validators';
 import { generateTSComponent } from 'src/app/shared/generateTSComponent';

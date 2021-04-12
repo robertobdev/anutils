550be9ab21ac4b47af34577eb8da6869efbac4c8
Roberto Barbosa <robertojf@gmail.com>
2021-04-12 19:35:28 -0300

    Remove validators

---
+++ src/app/pages/validators/validators.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ValidatorsRoutingModule } from './validators-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ValidatorsRoutingModule
+  ]
+})
+export class ValidatorsModule { }
